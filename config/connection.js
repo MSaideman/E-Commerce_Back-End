baee9b500493d64ff3ca48003cb834e4527e9c7b
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-09-01 10:49:32 -0400

    added to index.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
-      dialect: 'mysql',
+      dialect: 'mysql2',
       dialectOptions: {
         decimalNumbers: true,
       },
--- models/index.js
+++ models/index.js
@@ -5,13 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey:'category_id',
+  onDelete:'CASCADE'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+  onDelete:'CASCADE'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey:'category_id',
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey:'category_id',
+})
 module.exports = {
   Product,
   Category,
