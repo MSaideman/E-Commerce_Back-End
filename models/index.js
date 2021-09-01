f6fe35014f7dfe6983ec9a6ff6477f447a460984
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-09-01 10:50:47 -0400

    updated foreign key for belongstomany

--- models/index.js
+++ models/index.js
@@ -17,12 +17,12 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey:'category_id',
+  foreignKey:'product_id',
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
   through: ProductTag,
-  foreignKey:'category_id',
+  foreignKey:'tag_id',
 })
 module.exports = {
   Product,
